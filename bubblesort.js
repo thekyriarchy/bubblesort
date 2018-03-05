2b72a1e77e3ed563a09065910380b0a473449b9c
Kẏra <kxra@riseup.net>
2018-03-05 12:30:59 -0500

    passed some tests!

--- bubblesort.js
+++ bubblesort.js
@@ -1,13 +1,26 @@
-//swap counter = 0;
-//comparison counter = 0;
-
-//include assertions about swapcounter and comparison counter
-
-
-
-function bubbleSort (array){
-  let continue = true;
-  if (!array.length){
-    return [];
+function bubbleSort(array) {
+  let sorted = array;
+  if (sorted.length > 1) {
+    for (let i = 0; i < sorted.length - 1; i++) {
+      if (toSwap(sorted[i], sorted[i + 1])) {
+        swap(sorted, i);
+      }
+    }
   }
+  return sorted;
+}
+
+function toSwap(elem1, elem2) {
+  if (elem1 > elem2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function swap(arr, index) {
+  let toRight = arr[index];
+  let toLeft = arr[index + 1];
+  arr[index] = toLeft;
+  arr[index + 1] = toRight;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,25 +1,24 @@
-describe('Bubble Sort', function(){
-
-
-  beforeAll(function () {
-    spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
+describe("Bubble Sort", function() {
+  beforeAll(function() {
+    spyOn(window, "swap").and.callThrough(); // replace existing `swap`
   });
-  it('getting to the center of tootsiepop involves exactly three licks', function () {
-    tootsiepop.getToCenter();
-    expect(tootsiepop.lick.calls.count()).toEqual(3);
+  it("handles an empty array", function() {
+    expect(bubbleSort([])).toEqual([]);
   });
-
-
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+  it("handles a sorted array of integers", function() {
+    expect(bubbleSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
   });
-  it('handles a sorted array of integers', function () {
-    expect ( bubbleSort[1,2,3,4,5]).toEqual([1,2,3,4,5]);
+  it("handles a unsorted array of integers", function() {
+    expect(bubbleSort([2, 1, 3, 5, 4])).toEqual([1, 2, 3, 4, 5]);
+    expect(swap).toHaveBeenCalledTimes(2);
   });
-  it('handles a unsorted array of integers', function () {
-    expect ( bubbleSort[2,1,3,5,4]).toEqual([1,2,3,4,5]);
-  });
-  it('handles a decimal numbers', function () {
-    expect ( bubbleSort[2.5,1.3,3.1,5.6,4.1]).toEqual([1.3,2.5,3.1,4.1,5.6]);
+  it("handles a decimal numbers", function() {
+    expect(bubbleSort([2.5, 1.3, 3.1, 5.6, 4.1])).toEqual([
+      1.3,
+      2.5,
+      3.1,
+      4.1,
+      5.6
+    ]);
   });
 });
