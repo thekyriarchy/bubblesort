14cf6aeef4e12d32a27a57439888257fc7f82952
Horacio Gutierrez <hogutier@yahoo.com>
2018-03-05 15:00:28 -0600

    Completed Merge

--- mergesort.js
+++ mergesort.js
@@ -9,30 +9,37 @@ function merge(arr1, arr2) {
   let index1 = 0;
   let index2 = 0;
   let merged = [];
-  if (!arr1.length) {
-    while (index2 < arr2.length) {
-      merged.push(arr2[index2]);
-      index2++;
-    }
-  } else if (!arr2.length) {
-    while (index1 < arr1.length) {
-      merged.push(arr1[index1]);
-      index1++;
-    }
-  } else {
-    if (arr1[index1] < arr2[index2]) {
-      merged.push(arr1[index1]);
-      index1++;
+
+  while (index1 < arr1.length || index2 < arr2.length){
+    if (index1 === arr1.length) {
+      while (index2 < arr2.length) {
+        merged.push(arr2[index2]);
+        index2++;
+      }
+    } else if (index2 === arr2.length) {
+      while (index1 < arr1.length) {
+        merged.push(arr1[index1]);
+        index1++;
+      }
     } else {
-      merged.push(arr2[index2]);
-      index2++;
-    }
+      while (index2 < arr2.length && index1 < arr1.length){
+        if (arr1[index1] < arr2[index2]) {
+          merged.push(arr1[index1]);
+          index1++;
+        } else {
+          merged.push(arr2[index2]);
+          index2++;
+        }
+      }
+  }
   }
   return merged;
 }
 
-// function mergeSort(arr1, arr2) {
-//   if (!arr2 && !arr.length) {
-//
-//   }
-// }
+ function mergeSort(arr) {
+   if (arr.length < 2) {
+     return arr;
+   } else {
+     if (split(arr[(arr).length split)
+   }
+ }
